656a6c37e5f889a45e00058aca4e18613c84f8b0
Kevin <keverw92@gmail.com>
2012-06-02 18:56:37 -0400

    added encode and decode

--- HTTPRequest.js
+++ HTTPRequest.js
@@ -8,7 +8,7 @@ var HTTPRequest = {
         };
 
         var request_parms = this._mergeobjs(options, parameters);
-        
+
         this.request(url, request_parms, callback, options);
     },
     get: function (url, callback, options)
@@ -29,18 +29,18 @@ var HTTPRequest = {
         {
             parameters.method = 'GET';
         }
-        
+
         //post_data
         if (typeof parameters.post_data != 'undefined')
         {
-        	if (typeof parameters.post_data == 'object')
-        	{
-        		console.log(parameters.post_data);
-        		parameters.post_data = '';
-        	}
+            if (typeof parameters.post_data == 'object')
+            {
+                console.log(parameters.post_data);
+                parameters.post_data = '';
+            }
         }
 
-		//do HTML
+        //do HTML
         var xhr = this._getXHR();
         if (xhr == null) //NO XHR :(
         {
@@ -64,41 +64,102 @@ var HTTPRequest = {
                     xhr.setRequestHeader('User-Agent', parameters.useragent);
                 }
             }
-            
+
             if (parameters.method == 'POST')
             {
-            	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            	if (typeof parameters.post_data != 'undefined')
-            	{
-            		xhr.setRequestHeader("Content-length", parameters.post_data.length);
-            	}
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                if (typeof parameters.post_data != 'undefined')
+                {
+                    xhr.setRequestHeader("Content-length", parameters.post_data.length);
+                }
             }
 
-			if (parameters.method == 'POST')
+            if (parameters.method == 'POST')
             {
-            	if (typeof parameters.post_data != 'undefined')
-            	{
-            		xhr.send(parameters.post_data);
-            	}
-            	else
-            	{
-            		xhr.send();
-            	}
+                if (typeof parameters.post_data != 'undefined')
+                {
+                    xhr.send(parameters.post_data);
+                }
+                else
+                {
+                    xhr.send();
+                }
             }
             else
             {
-            	xhr.send();
+                xhr.send();
             }
         }
 
     },
-    encode: function (url)
+    encode: function (str)
     {
-        return 'later!';
+        // URL-encodes string  
+        // 
+        // version: 1109.2015
+        // discuss at: http://phpjs.org/functions/urlencode
+        // +   original by: Philip Peterson
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +      input by: AJ
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   improved by: Brett Zamir (http://brett-zamir.me)
+        // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +      input by: travc
+        // +      input by: Brett Zamir (http://brett-zamir.me)
+        // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   improved by: Lars Fischer
+        // +      input by: Ratheous
+        // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+        // +   bugfixed by: Joris
+        // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+        // %          note 1: This reflects PHP 5.3/6.0+ behavior
+        // %        note 2: Please be aware that this function expects to encode into UTF-8 encoded strings, as found on
+        // %        note 2: pages served as UTF-8
+        // *     example 1: urlencode('Kevin van Zonneveld!');
+        // *     returns 1: 'Kevin+van+Zonneveld%21'
+        // *     example 2: urlencode('http://kevin.vanzonneveld.net/');
+        // *     returns 2: 'http%3A%2F%2Fkevin.vanzonneveld.net%2F'
+        // *     example 3: urlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
+        // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
+        str = (str + '').toString();
+
+        // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+        // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+        return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+        replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
     },
-    decode: function (url)
+    decode: function (str)
     {
-        return 'later!';
+        // Decodes URL-encoded string  
+        // 
+        // version: 1109.2015
+        // discuss at: http://phpjs.org/functions/urldecode
+        // +   original by: Philip Peterson
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +      input by: AJ
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   improved by: Brett Zamir (http://brett-zamir.me)
+        // +      input by: travc
+        // +      input by: Brett Zamir (http://brett-zamir.me)
+        // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   improved by: Lars Fischer
+        // +      input by: Ratheous
+        // +   improved by: Orlando
+        // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+        // +      bugfixed by: Rob
+        // +      input by: e-mike
+        // +   improved by: Brett Zamir (http://brett-zamir.me)
+        // %        note 1: info on what encoding functions to use from: http://xkr.us/articles/javascript/encode-compare/
+        // %        note 2: Please be aware that this function expects to decode from UTF-8 encoded strings, as found on
+        // %        note 2: pages served as UTF-8
+        // *     example 1: urldecode('Kevin+van+Zonneveld%21');
+        // *     returns 1: 'Kevin van Zonneveld!'
+        // *     example 2: urldecode('http%3A%2F%2Fkevin.vanzonneveld.net%2F');
+        // *     returns 2: 'http://kevin.vanzonneveld.net/'
+        // *     example 3: urldecode('http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a');
+        // *     returns 3: 'http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a'
+        return decodeURIComponent((str + '').replace(/\+/g, '%20'));
+
     },
     //Private
     _headersToHeaders: function (headers_str)
