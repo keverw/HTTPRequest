c5c405d0f31cd8acdf1cc6e46f6c4d0c77b665c9
Kevin <keverw92@gmail.com>
2012-06-02 11:54:08 -0400

    Basic get client side test!

--- server_tests/get.js
+++ server_tests/get.js
@@ -1,6 +1,6 @@
-var http_client = require('../HTTPRequest.js');
+var HTTPRequest = require('../HTTPRequest.js');
 
-http_client.get('http://localhost/get_test.php', function(status, headers, content)
+HTTPRequest.get('http://localhost/get_test.php', function(status, headers, content)
 {
 	console.log(status, headers, content)
 });
