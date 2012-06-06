2e26fbf5a2d88afbb15d812a65b48db196efe972
Kevin <keverw92@gmail.com>
2012-06-06 02:36:44 -0400

    JSHinted and indented.

--- server_tests/post.js
+++ server_tests/post.js
@@ -1,6 +1,8 @@
 var HTTPRequest = require('../HTTPRequest.js');
 
-HTTPRequest.post('http://localhost/post_test.php', {hello: 'world'} ,function(status, headers, content)
+HTTPRequest.post('http://localhost/post_test.php', {
+    hello: 'world'
+}, function (status, headers, content)
 {
-	console.log(status, headers, content)
+    console.log(status, headers, content);
 });
