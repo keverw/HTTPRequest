24ebea765e37cd3f08c6d6ec698afe15c72ac6da
Kevin <keverw92@gmail.com>
2012-06-06 02:30:31 -0400

    JSHinted.

--- server_tests/get.js
+++ server_tests/get.js
@@ -2,5 +2,5 @@ var HTTPRequest = require('../HTTPRequest.js');
 
 HTTPRequest.get('http://localhost/get_test.php', function(status, headers, content)
 {
-	console.log(status, headers, content)
+	console.log(status, headers, content);
 },{QUERY: {pie: 'pumpkin', eat: 'Yes'}});
